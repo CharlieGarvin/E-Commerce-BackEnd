f6aba9da77c52505c2868795dae54bb328b9001b
unknown <charliegarvin90@gmail.com>
2023-05-23 17:00:18 -0600

    added all the models and finishing the index file for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
 
+  },
+});
 // Categories have many Products
+Category.hasMany(Product, {
+
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  }
+})
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  }
+});
+
 
 module.exports = {
   Product,
